5f94464f64a615379302775e54b0fe86bc18cc2a
ft <ft@noemail>
2016-12-27 13:03:36 +0100

    merged differing pages by adding webif.modules request

---
+++ adminpages/js/silversim/login.js
@@ -0,0 +1,298 @@
+// SilverSim is distributed under the terms of the
+// GNU Affero General Public License v3
+
+/******************************************************************************/
+function processLogin(e)
+{
+	require(["dojo/request","dojox/mobile/TransitionEvent","dijit/registry", "dojo/_base/array", "dojo/json"],
+		processLoginStep1);
+}
+
+/******************************************************************************/
+function processLoginStep1(request, TransitionEvent, registry, array)
+{
+	var username = registry.byId('user').get('value');
+	request("/admin/json", 
+	{
+		method:"POST",
+		data: JSON.stringify(
+		{ 
+			"method":"challenge",
+			"user":username
+		}),
+		headers:
+		{
+			"Content-Type":"application/json"
+		},
+		handleAs:"json"
+	}).then(
+		function(challenge_data) 
+		{
+			processLoginStep2(request, TransitionEvent, registry, array, challenge_data);
+		},
+		function(err) {
+		}
+	);
+}
+
+/******************************************************************************/
+function processLoginStep2(request, TransitionEvent, registry, array, challenge_data)
+{
+	var username = registry.byId('user').get('value');
+	var password = registry.byId('pass').get('value');
+	if(!challenge_data.success)
+	{
+		showErrorDialog(challenge_data.reason);
+		return;
+	}
+	
+	response = generateResponse(challenge_data.challenge, password);
+	pass.value = "";
+	user.value = "";
+	sessionid = challenge_data.sessionid;
+	
+	request("/admin/json",
+	{
+		method:"POST",
+		data: JSON.stringify(
+		{
+			"method":"login",
+			"user":username,
+			"response":response,
+			"sessionid":challenge_data.sessionid
+		}),
+		headers:
+		{
+			"Content-Type":"application/json"
+		},
+		handleAs:"json"
+	}).then(
+		function(login_data) 
+		{
+			processLoginStep3(request, TransitionEvent, registry, array, login_data);
+		},
+		function(err)
+		{
+		}
+	);
+}
+
+/******************************************************************************/
+function processLoginStep3(request, TransitionEvent, registry, array, login_data)
+{
+	request("/admin/json", 
+	{
+		method:"POST",
+		data: JSON.stringify(
+		{ 
+			"method":"webif.modules",
+			"sessionid":sessionid
+		}),
+		headers:
+		{
+			"Content-Type":"application/json"
+		},
+		handleAs:"json"
+	}).then(
+		function(modules_data) 
+		{
+			processLoginStep4(request, TransitionEvent, registry, array, login_data, modules_data);
+		},
+		function(err) {
+			processLogout(0);
+		}
+	);
+}
+
+/******************************************************************************/
+function processLoginStep4(request, TransitionEvent, registry, array, login_data, modules_data)
+{
+	if(!login_data.success)
+	{
+		showErrorDialog(login_data.reason);
+		return;
+	}
+	
+	var mainview = registry.byId("view_main");
+	mainview.destroyDescendants();
+	
+	rights = login_data.rights;
+	containsAdminAll = array.indexOf(login_data.rights, "admin.all") >= 0;
+	var childWidget;
+	var sectionWidget;
+	
+	var have_regions_notice = (containsAdminAll || array.indexOf(login_data.rights, "regions.notice")>=0) && array.indexOf(modules_data.modules, "regions")>=0;
+	
+	if( containsAdminAll ||
+		(array.indexOf(login_data.rights, "issues.view")>=0 && login_data.numissues != 0) ||
+		have_regions_notice ||
+		array.indexOf(login_data.rights, "modules.view")>=0)
+	{
+		mainview.addChild(new dojox.mobile.RoundRectCategory({label:"Simulator"}));
+		var list = new dojox.mobile.RoundRectList();
+		mainview.addChild(list);
+	
+		if(containsAdminAll || array.indexOf(login_data.rights, "issues.view")>=0)
+		{
+			if(login_data.numissues != 0)
+			{
+				
+				var numIssues = login_data.numissues != 0 ? login_data.numissues : "None";
+				childWidget = new dojox.mobile.ListItem({
+					clickable:true,
+					rightText:numIssues,
+					label:"Configuration Issues"});
+				list.addChild(childWidget);
+				childWidget.on("click", switchToConfigurationIssues);
+			}
+		}
+		
+		if(containsAdminAll || array.indexOf(login_data.rights, "modules.view")>=0)
+		{
+				childWidget = new dojox.mobile.ListItem({
+					clickable:true,
+					label:"Configured Modules"});
+				list.addChild(childWidget);
+				childWidget.on("click", switchToModulesList);
+		}
+		
+		if(have_regions_notice)
+		{
+			var listItem;
+			listItem = new dojox.mobile.ListItem({
+					label:'Send Simulator Notice',
+					onclick:"dijit.registry.byId('simulatornoticedialog').show()",
+					clickable:true,
+					arrowClass:'mblDomButtonGrayKnob'});
+			list.addChild(listItem);
+		}
+	}
+	
+	if(array.indexOf(modules_data.modules, "useraccounts")>=0)
+	{
+		if(containsAdminAll || array.indexOf(login_data.rights, "useraccounts.view")>=0 ||
+			array.indexOf(login_data.rights, "useraccounts.create") >= 0 ||
+			array.indexOf(login_data.rights, "useraccounts.delete") >= 0 ||
+			array.indexOf(login_data.rights, "useraccounts.manage") >= 0)
+		{
+			mainview.addChild(new dojox.mobile.RoundRectCategory({label:"Accounts"}));
+			var list = new dojox.mobile.RoundRectList();
+			mainview.addChild(list);
+			
+			if(containsAdminAll || array.indexOf(login_data.rights, "useraccounts.create")>=0)
+			{
+				childWidget = new dojox.mobile.ListItem({
+					clickable:true,
+					label:"Add Account",
+					moveTo:"viewaccountadd",
+					transition:"slide"});
+				list.addChild(childWidget);
+			}
+			childWidget = new dojox.mobile.ListItem({
+				clickable:true,
+				label:"List"});
+			childWidget.on("click", function() { switchToAccounts(1, viewmain); });
+			list.addChild(childWidget);
+		}
+	}
+	
+	if(array.indexOf(modules_data.modules, "estates")>=0)
+	{
+		if(containsAdminAll || array.indexOf(login_data.rights, "estates.view")>=0)
+		{
+			mainview.addChild(new dojox.mobile.RoundRectCategory({label:"Estates"}));
+			var list = new dojox.mobile.RoundRectList();
+			mainview.addChild(list);
+			childWidget = new dojox.mobile.ListItem({
+				clickable:true,
+				label:"List"});
+			list.addChild(childWidget);
+			childWidget.on("click", function() {switchToEstatesList(1,viewmain); });
+		}
+	}
+	
+	if(array.indexOf(modules_data.modules, "regions")>=0)
+	{
+		if(containsAdminAll ||
+			array.indexOf(login_data.rights, "regions.control")>=0 ||
+			array.indexOf(login_data.rights, "regions.manage") >= 0 ||
+			array.indexOf(login_data.rights, "regions.logincontrol")>=0 ||
+			array.indexOf(login_data.rights, "regions.notice")>=0 ||
+			array.indexOf(login_data.rights, "regions.agents.view")>=0)
+		{
+			mainview.addChild(new dojox.mobile.RoundRectCategory({label:"Regions"}));
+			var list = new dojox.mobile.RoundRectList();
+			mainview.addChild(list);
+			
+			childWidget = new dojox.mobile.ListItem({
+				clickable:true,
+				label:"List"});
+			list.addChild(childWidget);
+			childWidget.on("click", function() { switchToRegionsList(1, viewmain); });
+			
+			if(containsAdminAll || array.indexOf(login_data.rights, "regions.control")>=0)
+			{
+				childWidget = new dojox.mobile.ListItem({
+					clickable:true,
+					label:"Start/Stop"});
+				list.addChild(childWidget);
+				childWidget.on("click", switchToRegionControl);
+			}
+			
+			if(containsAdminAll || array.indexOf(login_data.rights, "regions.manage")>=0)
+			{
+				childWidget = new dojox.mobile.ListItem({
+					clickable:true,
+					label:"Auto-Start Enable"});
+				list.addChild(childWidget);
+				childWidget.on("click", switchToRegionEnable);
+			}
+			
+			if(containsAdminAll || array.indexOf(login_data.rights, "regions.logincontrol")>=0)
+			{
+				childWidget = new dojox.mobile.ListItem({
+					clickable:true,
+					label:"Login Enable"});
+				list.addChild(childWidget);
+				childWidget.on("click", switchToLoginControl);
+			}
+			
+			if(containsAdminAll ||array.indexOf(login_data.rights, "regions.agents.view")>=0)
+			{
+				childWidget = new dojox.mobile.ListItem({
+					clickable:true,
+					label:"Agents List per Region"});
+				list.addChild(childWidget);
+				childWidget.on("click", function() { switchToAgentRegionsList(1, viewmain);});
+			}
+		}
+	}
+	
+	if(array.indexOf(modules_data.modules, "console")>=0)
+	{
+		if(containsAdminAll || array.indexOf(login_data.rights, "console.access")>=0)
+		{
+			mainview.addChild(new dojox.mobile.RoundRectCategory({label:"Console"}));
+			var list = new dojox.mobile.RoundRectList();
+			mainview.addChild(list);
+			
+			childWidget = new dojox.mobile.ListItem({
+				clickable:true,
+				label:">>",
+				moveTo:"viewconsole",
+				transition:"slide"});
+			list.addChild(childWidget);
+		}
+	}
+	
+	initEstateDetails();
+	initRegionDetails();
+	initAgentDetails();
+	initAccountDetails();
+	
+	new TransitionEvent(viewlogin, {
+		moveTo: "viewmain",
+		transition: "slide",
+		transitionDir: 1
+	}).dispatch();
+}
