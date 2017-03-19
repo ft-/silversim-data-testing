72a720dc2cb17e6142f7d2ab41c700fa3c35ebcf
ft <ft@noemail>
2017-03-19 12:44:08 +0100

    updated packages.js

--- adminpages/js/silversim/packages.js
+++ adminpages/js/silversim/packages.js
@@ -61,49 +61,49 @@ function initPackageAdmin()
 /******************************************************************************/
 function switchToInstalledPackages()
 {
-	require(["dojo/_base/array", "dojo/request", "dijit/registry", "dojox/mobile/TransitionEvent"], 
-		function(array, request, registry, TransitionEvent)
-	{
-		list = registry.byId("list_installedpackages");
-		list.destroyDescendants();
-		request("/admin/json", 
-		{
-			method:"POST",
-			data: JSON.stringify(
-			{ 
-				"method":"packages.list.installed",
-				"sessionid":sessionid
-			}),
-			headers:
-			{
-				"Content-Type":"application/json"
-			},
-			handleAs:"json"
-		}).then(
-			function(data) 
-			{
-				if(!data.success)
-				{
-					if(data.reason == 1)
-					{
-						new TransitionEvent(packageadmin, {
-							moveTo: "viewlogin",
-							transition: "slide",
-							transitionDir: -1
-						}).dispatch();
-					}
-					else
-					{
-						showErrorDialog(data.reason);
-					}
-					return;
-				}
-				
-				var hasUninstallRight = containsAdminAll ||
-					array.indexOf("packages.uninstall")>=0;
-				
-				array.forEach(data.list, function(pack)
-				{
+    require(["dojo/_base/array", "dojo/request", "dijit/registry", "dojox/mobile/TransitionEvent"], 
+        function(array, request, registry, TransitionEvent)
+    {
+        list = registry.byId("list_installedpackages");
+        list.destroyDescendants();
+        request("/admin/json", 
+        {
+            method:"POST",
+            data: JSON.stringify(
+            { 
+                "method":"packages.list.installed",
+                "sessionid":sessionid
+            }),
+            headers:
+            {
+                "Content-Type":"application/json"
+            },
+            handleAs:"json"
+        }).then(
+            function(data) 
+            {
+                if(!data.success)
+                {
+                    if(data.reason == 1)
+                    {
+                        new TransitionEvent(packageadmin, {
+                            moveTo: "viewlogin",
+                            transition: "slide",
+                            transitionDir: -1
+                        }).dispatch();
+                    }
+                    else
+                    {
+                        showErrorDialog(data.reason);
+                    }
+                    return;
+                }
+                
+                var hasUninstallRight = containsAdminAll ||
+                    array.indexOf(rights, "packages.uninstall")>=0;
+                
+                array.forEach(data.list, function(pack)
+                {
                     var childWidget = new dojox.mobile.ListItem({
                         id:"pkg_" + pack.name,
                         rightText:pack.version,
@@ -115,186 +115,245 @@ function switchToInstalledPackages()
                     {
                         childWidget.on("click", function() { uninstallPackage(pack.name);});
                     }
-				});
-				
-				new TransitionEvent(packageadmin, {
-					moveTo: "viewinstalledpackages",
-					transition: "slide",
-					transitionDir: 1
-				}).dispatch();
-			},
-			function(err) {
-				showErrorTextDialog(err.toString());
-			}
-		);
-	});
+                });
+                
+                new TransitionEvent(packageadmin, {
+                    moveTo: "viewinstalledpackages",
+                    transition: "slide",
+                    transitionDir: 1
+                }).dispatch();
+            },
+            function(err) {
+                showErrorTextDialog(err.toString());
+            }
+        );
+    });
 }
 
 /******************************************************************************/
 function switchToAvailablePackages()
 {
-	require(["dojo/_base/array", "dojo/request", "dijit/registry", "dojox/mobile/TransitionEvent"], 
-		function(array, request, registry, TransitionEvent)
-	{
-		list = registry.byId("list_installedpackages");
-		list.destroyDescendants();
-		request("/admin/json", 
-		{
-			method:"POST",
-			data: JSON.stringify(
-			{ 
-				"method":"packages.list.available",
-				"sessionid":sessionid
-			}),
-			headers:
-			{
-				"Content-Type":"application/json"
-			},
-			handleAs:"json"
-		}).then(
-			function(data) 
-			{
-				if(!data.success)
-				{
-					if(data.reason == 1)
-					{
-						new TransitionEvent(packageadmin, {
-							moveTo: "viewlogin",
-							transition: "slide",
-							transitionDir: -1
-						}).dispatch();
-					}
-					else
-					{
-						showErrorDialog(data.reason);
-					}
-					return;
-				}
-				
-				var hasInstallRight = containsAdminAll ||
-					array.indexOf("packages.install")>=0;
-				
-				array.forEach(data.list, function(pack)
-				{
-                    var childWidget = new dojox.mobile.ListItem({
-                        id:"pkg_" + pack.name,
-                        rightText:pack.version,
-                        clickable:hasInstallRight,
-                        arrowClass:'mblDomButtonGreenCirclePlus',
-                        label:pack.name});
-                    list.addChild(childWidget);
-                    if(hasUninstallRight)
+    var installedpackages = []
+    require(["dojo/_base/array", "dojo/request", "dijit/registry", "dojox/mobile/TransitionEvent"], 
+        function(array, request, registry, TransitionEvent)
+    {
+        list = registry.byId("list_installedpackages");
+        list.destroyDescendants();
+        request("/admin/json", 
+        {
+            method:"POST",
+            data: JSON.stringify(
+            { 
+                "method":"packages.list.installed",
+                "sessionid":sessionid
+            }),
+            headers:
+            {
+                "Content-Type":"application/json"
+            },
+            handleAs:"json"
+        }).then(
+            function(data) 
+            {
+                if(!data.success)
+                {
+                    if(data.reason == 1)
                     {
-                        childWidget.on("click", function() { installPackage(pack.name);});
+                        new TransitionEvent(packageadmin, {
+                            moveTo: "viewlogin",
+                            transition: "slide",
+                            transitionDir: -1
+                        }).dispatch();
                     }
-				});
-				
-				new TransitionEvent(packageadmin, {
-					moveTo: "viewinstalledpackages",
-					transition: "slide",
-					transitionDir: 1
-				}).dispatch();
-			},
-			function(err) {
-				showErrorTextDialog(err.toString());
-			}
-		);
-	});
+                    else
+                    {
+                        showErrorDialog(data.reason);
+                    }
+                    return;
+                }
+                
+                var hasUninstallRight = containsAdminAll ||
+                    array.indexOf("packages.uninstall")>=0;
+                
+                array.forEach(data.list, function(pack)
+                {
+                    installedpackages.push(pack.name);
+                });
+                
+                list = registry.byId("list_availablepackages");
+                list.destroyDescendants();
+                request("/admin/json", 
+                {
+                    method:"POST",
+                    data: JSON.stringify(
+                    { 
+                        "method":"packages.list.available",
+                        "sessionid":sessionid
+                    }),
+                    headers:
+                    {
+                        "Content-Type":"application/json"
+                    },
+                    handleAs:"json"
+                }).then(
+                    function(data) 
+                    {
+                        if(!data.success)
+                        {
+                            if(data.reason == 1)
+                            {
+                                new TransitionEvent(packageadmin, {
+                                    moveTo: "viewlogin",
+                                    transition: "slide",
+                                    transitionDir: -1
+                                }).dispatch();
+                            }
+                            else
+                            {
+                                showErrorDialog(data.reason);
+                            }
+                            return;
+                        }
+                        
+                        var hasInstallRight = containsAdminAll ||
+                            array.indexOf(rights, "packages.install")>=0;
+                        
+                        array.forEach(data.list, function(pack)
+                        {
+                            var actArrowClass = 'mblDomButtonGreenCirclePlus';
+                            if(array.indexOf(installedpackages, pack.name)>=0)
+                            {
+                                actArrowClass = 'mblDomButtonBlueCirclePlus';
+                            }
+                            var childWidget = new dojox.mobile.ListItem({
+                                id:"pkginst_" + pack.name,
+                                rightText:pack.version,
+                                clickable:hasInstallRight,
+                                arrowClass:actArrowClass,
+                                label:pack.name});
+                            list.addChild(childWidget);
+                            if(hasInstallRight)
+                            {
+                                childWidget.on("click", function() { installPackage(pack.name);});
+                            }
+                        });
+                        
+                        new TransitionEvent(packageadmin, {
+                            moveTo: "viewavailablepackages",
+                            transition: "slide",
+                            transitionDir: 1
+                        }).dispatch();
+                    },
+                    function(err) {
+                        showErrorTextDialog(err.toString());
+                    }
+                );
+            },
+            function(err) {
+                showErrorTextDialog(err.toString());
+            }
+        );
+    });
 }
 
 /******************************************************************************/
 function updatePackageFeed(pkgid)
 {
-	require(["dojo/_base/array", "dojo/request", "dijit/registry", "dojox/mobile/TransitionEvent"], 
-		function(array, request, registry, TransitionEvent)
-	{
-		list = registry.byId("list_installedpackages");
-		list.destroyDescendants();
-		request("/admin/json", 
-		{
-			method:"POST",
-			data: JSON.stringify(
-			{ 
-				"method":"packages.update.feed",
-				"sessionid":sessionid
-			}),
-			headers:
-			{
-				"Content-Type":"application/json"
-			},
-			handleAs:"json"
-		}).then(
-			function(data) 
-			{
-				if(!data.success)
-				{
-					if(data.reason == 1)
-					{
-						new TransitionEvent(packageadmin, {
-							moveTo: "viewlogin",
-							transition: "slide",
-							transitionDir: -1
-						}).dispatch();
-					}
-					else
-					{
-						showErrorDialog(data.reason);
-					}
-					return;
-				}
-			},
-			function(err) {
-				showErrorTextDialog(err.toString());
-			}
-		);
-	});
+    require(["dojo/_base/array", "dojo/request", "dijit/registry", "dojox/mobile/TransitionEvent"], 
+        function(array, request, registry, TransitionEvent)
+    {
+        list = registry.byId("list_installedpackages");
+        list.destroyDescendants();
+        request("/admin/json", 
+        {
+            method:"POST",
+            data: JSON.stringify(
+            { 
+                "method":"packages.update.feed",
+                "sessionid":sessionid
+            }),
+            headers:
+            {
+                "Content-Type":"application/json"
+            },
+            handleAs:"json"
+        }).then(
+            function(data) 
+            {
+                if(!data.success)
+                {
+                    if(data.reason == 1)
+                    {
+                        new TransitionEvent(packageadmin, {
+                            moveTo: "viewlogin",
+                            transition: "slide",
+                            transitionDir: -1
+                        }).dispatch();
+                    }
+                    else
+                    {
+                        showErrorDialog(data.reason);
+                    }
+                    return;
+                }
+            },
+            function(err) {
+                showErrorTextDialog(err.toString());
+            }
+        );
+    });
 }
 
 /******************************************************************************/
 function installPackage(pkgid)
 {
-	require(["dojo/_base/array", "dojo/request", "dijit/registry", "dojox/mobile/TransitionEvent"], 
-		function(array, request, registry, TransitionEvent)
-	{
-		request("/admin/json", 
-		{
-			method:"POST",
-			data: JSON.stringify(
-			{ 
-				"method":"package.install",
+    require(["dojo/_base/array", "dojo/request", "dijit/registry", "dojox/mobile/TransitionEvent"], 
+        function(array, request, registry, TransitionEvent)
+    {
+        request("/admin/json", 
+        {
+            method:"POST",
+            data: JSON.stringify(
+            { 
+                "method":"package.install",
                 "package":pkgid,
-				"sessionid":sessionid
-			}),
-			headers:
-			{
-				"Content-Type":"application/json"
-			},
-			handleAs:"json"
-		}).then(
-			function(data) 
-			{
-				if(!data.success)
-				{
-					if(data.reason == 1)
-					{
-						new TransitionEvent(packageadmin, {
-							moveTo: "viewlogin",
-							transition: "slide",
-							transitionDir: -1
-						}).dispatch();
-					}
-					else
-					{
-						showErrorDialog(data.reason);
-					}
-					return;
-				}
-			},
-			function(err) {
-				showErrorTextDialog(err.toString());
-			}
-		);
-	});
+                "sessionid":sessionid
+            }),
+            headers:
+            {
+                "Content-Type":"application/json"
+            },
+            handleAs:"json"
+        }).then(
+            function(data) 
+            {
+                if(!data.success)
+                {
+                    if(data.reason == 1)
+                    {
+                        new TransitionEvent(packageadmin, {
+                            moveTo: "viewlogin",
+                            transition: "slide",
+                            transitionDir: -1
+                        }).dispatch();
+                    }
+                    else
+                    {
+                        showErrorDialog(data.reason);
+                    }
+                    return;
+                }
+                else
+                {
+                    var item = registry.byId("pkginst_" + pkgid);
+                    item.setArrow('mblDomButtonBlueCirclePlus');
+                }
+            },
+            function(err) {
+                showErrorTextDialog(err.toString());
+            }
+        );
+    });
 }
 
 /******************************************************************************/
@@ -302,59 +361,59 @@ var selectedpackage;
 
 function uninstallPackage(pkgid)
 {
-	require(["dijit/registry"], 
-		function(registry)
-	{
+    require(["dijit/registry"], 
+        function(registry)
+    {
         selectedpackage = pkgid;
         registry.byId('confirmpackageuninstalldialog').show();
-	});
+    });
 }
 
 /******************************************************************************/
 function uninstallSelectedPackage()
 {
-	require(["dojo/_base/array", "dojo/request", "dijit/registry", "dojox/mobile/TransitionEvent"], 
-		function(array, request, registry, TransitionEvent)
-	{
+    require(["dojo/_base/array", "dojo/request", "dijit/registry", "dojox/mobile/TransitionEvent"], 
+        function(array, request, registry, TransitionEvent)
+    {
         registry.byId('confirmpackageuninstalldialog').hide();
-		list = registry.byId("list_installedpackages");
-		request("/admin/json", 
-		{
-			method:"POST",
-			data: JSON.stringify(
-			{ 
-				"method":"package.uninstall",
+        list = registry.byId("list_installedpackages");
+        request("/admin/json", 
+        {
+            method:"POST",
+            data: JSON.stringify(
+            { 
+                "method":"package.uninstall",
                 "package":selectedpackage,
-				"sessionid":sessionid
-			}),
-			headers:
-			{
-				"Content-Type":"application/json"
-			},
-			handleAs:"json"
-		}).then(
-			function(data) 
-			{
-				if(!data.success)
-				{
-					if(data.reason == 1)
-					{
-						new TransitionEvent(packageadmin, {
-							moveTo: "viewlogin",
-							transition: "slide",
-							transitionDir: -1
-						}).dispatch();
-					}
-					else
-					{
-						showErrorDialog(data.reason);
-					}
-					return;
-				}
-			},
-			function(err) {
-				showErrorTextDialog(err.toString());
-			}
-		);
-	});
+                "sessionid":sessionid
+            }),
+            headers:
+            {
+                "Content-Type":"application/json"
+            },
+            handleAs:"json"
+        }).then(
+            function(data) 
+            {
+                if(!data.success)
+                {
+                    if(data.reason == 1)
+                    {
+                        new TransitionEvent(packageadmin, {
+                            moveTo: "viewlogin",
+                            transition: "slide",
+                            transitionDir: -1
+                        }).dispatch();
+                    }
+                    else
+                    {
+                        showErrorDialog(data.reason);
+                    }
+                    return;
+                }
+            },
+            function(err) {
+                showErrorTextDialog(err.toString());
+            }
+        );
+    });
 }
